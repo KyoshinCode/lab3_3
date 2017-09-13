648ef9ab33ecbdb8ea23c8c8bae96a670698c272
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-09-14 00:28:46 +0200

    Added test which check if order is expired

--- src/test/java/lab_3_3/OrderTest.java
+++ src/test/java/lab_3_3/OrderTest.java
@@ -2,14 +2,23 @@ package lab_3_3;
 
 import org.junit.Test;
 
+import edu.iis.mto.time.CustomTime;
 import edu.iis.mto.time.Order;
+import edu.iis.mto.time.OrderExpiredException;
 import edu.iis.mto.time.SystemTime;
 
 public class OrderTest {
-	 @Test
+	@Test
      public void checkIfOrderIsNotExpired() throws Exception {
          Order order = new Order(new SystemTime());
          order.submit();
          order.confirm();
       }
+
+    @Test (expected = OrderExpiredException.class)
+    public void checkIfOrderIsExpired() throws Exception {
+        Order order = new Order(new CustomTime());
+        order.submit();
+        order.confirm();
+    }
 }
