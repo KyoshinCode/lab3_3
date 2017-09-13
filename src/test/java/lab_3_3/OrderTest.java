0e49c97cba67b552897bf4a1345344064305b0b1
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-09-14 00:54:07 +0200

    Added test for checking if order has been cancelled

--- src/test/java/lab_3_3/OrderTest.java
+++ src/test/java/lab_3_3/OrderTest.java
@@ -2,10 +2,13 @@ package lab_3_3;
 
 import org.junit.Test;
 
+import org.junit.Assert;
 import edu.iis.mto.time.CustomTime;
 import edu.iis.mto.time.Order;
 import edu.iis.mto.time.OrderExpiredException;
 import edu.iis.mto.time.SystemTime;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 
 public class OrderTest {
 	@Test
@@ -21,4 +24,12 @@ public class OrderTest {
         order.submit();
         order.confirm();
     }
+
+    @Test (expected = OrderExpiredException.class)
+     public void checkIfOrderHasBeenCancelled() throws Exception {
+         Order order = new Order(new CustomTime());
+         order.submit();;
+         order.confirm();
+         Assert.assertThat(order.getOrderState(),is(equalTo(Order.State.CANCELLED)));
+     }
 }
